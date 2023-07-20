9208807495398d965c1ac29c527e1ba50df724b7
impresence <nikitbreak@gmail.com>
2023-07-20 19:33:55 +0300

    test deploy

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-// const sdkID = 'ab75de33-db3d-4c4c-89ed-136adfa1b57b'
-// const clientId = '83ec1a35d73dd4ed'
-// const clientSecret = '83ec1a35d73dd4ed'
+const sdkID = "ab75de33-db3d-4c4c-89ed-136adfa1b57b";
+const clientId = "83ec1a35d73dd4ed";
+const clientSecret = "83ec1a35d73dd4ed";
 
-// const div = document.getElementById('div')
-// div.innerHTML = 'something'
+import AppExtensionsSDK from "@pipedrive/app-extensions-sdk";
 
-// const h1 = document.createElement('h1')
-// h1.innerHTML = 'header'
-// document.body.append(h1)
-
+const sdk = await new AppExtensionsSDK().initialize();
