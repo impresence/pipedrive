dc3841348e9f3c82a271dcdd44b5053af1914ccc
impresence <nikitbreak@gmail.com>
2023-07-20 20:14:21 +0300

    solved iframe initialization

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const sdkID = "ab75de33-db3d-4c4c-89ed-136adfa1b57b";
 const clientId = "83ec1a35d73dd4ed";
 const clientSecret = "83ec1a35d73dd4ed";
 
-const sdk = await new AppExtensionsSDK({ identifier: sdkID }).initialize({
-  size: { height: 500, width: 500 },
-});
+async function init() {
+  const sdk = await new AppExtensionsSDK({ identifier: sdkID }).initialize({
+    size: { height: 500, width: 500 },
+  });
+}
