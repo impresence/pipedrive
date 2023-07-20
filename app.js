be240e21bcbb3457618cc447022f5b67cadd9965
impresence <nikitbreak@gmail.com>
2023-07-20 17:41:17 +0300

    test 2

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const div = document.getElementById('div')
-div.innerHTML = 'something'
+// const sdkID = 'ab75de33-db3d-4c4c-89ed-136adfa1b57b'
+// const clientId = '83ec1a35d73dd4ed'
+// const clientSecret = '83ec1a35d73dd4ed'
 
-const h1 = document.createElement('h1')
-h1.innerHTML = 'header'
-document.body.append(h1)
+// const div = document.getElementById('div')
+// div.innerHTML = 'something'
+
+// const h1 = document.createElement('h1')
+// h1.innerHTML = 'header'
+// document.body.append(h1)
+
