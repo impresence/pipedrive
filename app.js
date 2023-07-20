de99081f0d7019a132f0c1e8a64760c5c9d1ebc3
impresence <nikitbreak@gmail.com>
2023-07-20 20:41:44 +0300

    commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const clientId = "83ec1a35d73dd4ed";
 const clientSecret = "83ec1a35d73dd4ed";
 
 (async function () {
-  const sdk = await new AppExtensionsSDK({ identifier: sdkID }).initialize({
+  const sdk = await new AppExtensionsSDK().initialize({
     size: { height: 500, width: 500 },
   });
   console.log("initialized");
