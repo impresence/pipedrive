34ce8c2ae5bf24854eeeae52602e6579de8a0d27
impresence <nikitbreak@gmail.com>
2023-07-20 20:40:37 +0300

    commit

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-import AppExtensionsSDK from "@pipedrive/app-extensions-sdk";
+const sdkID = "ab75de33-db3d-4c4c-89ed-136adfa1b57b";
+const clientId = "83ec1a35d73dd4ed";
+const clientSecret = "83ec1a35d73dd4ed";
 
-(async function() {
-  const sdk = await new AppExtensionsSDK().initialize();
-  console.log('initialized')
+(async function () {
+  const sdk = await new AppExtensionsSDK({ identifier: sdkID }).initialize({
+    size: { height: 500, width: 500 },
+  });
+  console.log("initialized");
 })();
