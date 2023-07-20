9bfb2d23f97864c6c3296aa4294d7c94205b86ff
impresence <nikitbreak@gmail.com>
2023-07-20 19:45:10 +0300

    deleted .then

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+import AppExtensionsSDK from "@pipedrive/app-extensions-sdk";
+
 const sdkID = "ab75de33-db3d-4c4c-89ed-136adfa1b57b";
 const clientId = "83ec1a35d73dd4ed";
 const clientSecret = "83ec1a35d73dd4ed";
-
-import AppExtensionsSDK from "@pipedrive/app-extensions-sdk";
 
 const sdk = await new AppExtensionsSDK({ identifier: sdkID }).initialize({
   size: { height: 500, width: 500 },
