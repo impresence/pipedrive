aa1fa762776a2fc0112545c468758a19c8189f29
impresence <nikitbreak@gmail.com>
2023-07-21 21:51:36 +0300

    markup

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const clientSecret = "83ec1a35d73dd4ed";
 
 (async function () {
   const sdk = await new AppExtensionsSDK().initialize({
-    size: { height: 500, width: 500 },
+    size: { height: 1000, width: 1000 },
   });
   console.log("initialized");
 })();
