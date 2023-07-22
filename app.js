e29ce27c0e9448e0981e44c9492573c3735e2077
impresence <nikitbreak@gmail.com>
2023-07-22 04:52:46 +0300

    access token

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 // const sdkID = "ab75de33-db3d-4c4c-89ed-136adfa1b57b";
 // const code = "11339253.15456594.a32ba4a3ad08321fa1ba057aa1f4d78fabdaabc0";
-// const auth = {
-//   access_token:
-//     "ERrRB3jAu+2lT3X/+M7YUcKOUMMZe4QUC57XxQys7b1RUbz16R23erc/KyOpYjfpH6Jvg7O6G+MXQJOgCyXa6FQneweSCtxFYP1MS/u8qa7hzknKrFXGyERMsUNO5VhlinV37xsSKjiFv0GX7DfrphGlQo75ktD9Lprim4GQeqsTRB6q670SJ6taFcU8alpxK1f7irtHLmKJC8Po4zKaSgBFLDK0d22Hr2rcvteg2P5y7ClCHSjr3IJ6iwKgZorD+p7+b/QrlpFiWAMaUCtTeTT5jRF0MQ==",
-//   token_type: "Bearer",
-//   expires_in: 3599,
-//   refresh_token: "11339253:15456594:859f5c5a6de370f64c8acaf818af45727d87ad5a",
-//   scope: "base,deals:full,users:read",
-//   api_domain: "https://salesautomators-sandbox8.pipedrive.com",
-// };
+const auth = {
+  access_token:
+    "/zqM1Ovy+CidJWcS2rX/mouiH3/sFuL8q+6gK1XIIhmenQEXxMxSRgnuRG+ihULZO01dnAE0yz49KlpWu6GREB7jWBTqNVop8iddEPVK6a8GRbZGrbm5x1LTmJOKGDQcdZCZ9YB7+aLVsLA7eC22UjHwWgCaC42ht12FSjkU7pO3yn/SrYwYIpsXMg4Kpb4ddQ1MD3mo3jZXWjer3m8I/BqE6eTvw8cSWPUeiJC/6KJILcCNF6G/XfED/HJM0z/fybL/8Ex3jnW/mlb2Ey3nIQKYH2yjMg==",
+  token_type: "Bearer",
+  expires_in: 3599,
+  refresh_token: "11339253:15456594:859f5c5a6de370f64c8acaf818af45727d87ad5a",
+  scope: "base,deals:full,users:read",
+  api_domain: "https://salesautomators-sandbox8.pipedrive.com",
+};
+
 const apiKey = "9120af394df9e7eb5f262b64fbf92690228a0cb6";
 const ClientId = "6799ba7898f04ad8";
 const ClientSecret = "78e892b74083d927953acf9f9cb46e2a943a4786";
@@ -25,7 +26,7 @@ const form = document.getElementById("container");
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const accessToken =
-    "ERrRB3jAu+2lT3X/+M7YUcKOUMMZe4QUC57XxQys7b1RUbz16R23erc/KyOpYjfpH6Jvg7O6G+MXQJOgCyXa6FQneweSCtxFYP1MS/u8qa7hzknKrFXGyERMsUNO5VhlinV37xsSKjiFv0GX7DfrphGlQo75ktD9Lprim4GQeqsTRB6q670SJ6taFcU8alpxK1f7irtHLmKJC8Po4zKaSgBFLDK0d22Hr2rcvteg2P5y7ClCHSjr3IJ6iwKgZorD+p7+b/QrlpFiWAMaUCtTeTT5jRF0MQ==";
+    "/zqM1Ovy+CidJWcS2rX/mouiH3/sFuL8q+6gK1XIIhmenQEXxMxSRgnuRG+ihULZO01dnAE0yz49KlpWu6GREB7jWBTqNVop8iddEPVK6a8GRbZGrbm5x1LTmJOKGDQcdZCZ9YB7+aLVsLA7eC22UjHwWgCaC42ht12FSjkU7pO3yn/SrYwYIpsXMg4Kpb4ddQ1MD3mo3jZXWjer3m8I/BqE6eTvw8cSWPUeiJC/6KJILcCNF6G/XfED/HJM0z/fybL/8Ex3jnW/mlb2Ey3nIQKYH2yjMg==";
   const apiUrl = "https://api.pipedrive.com/v1/deals";
 
   const firstName = document.getElementById("firstName").value;
